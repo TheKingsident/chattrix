91e09cb21652a5b6123a59f46d2235f67ba80fd6
TheKingsident <kingsident@gmail.com>
2024-11-04 13:12:18 +0100

    Refactor fetch_comments script to accept video ID as input and add main function for execution

--- src/utils/fetch_comments.py
+++ src/utils/fetch_comments.py
@@ -1,8 +1,10 @@
+#!/usr/bin/env python3
+
 import requests
 import json
 
 API_KEY = "AIzaSyD76HWjGjQ5F6j2tkExCg5Qpz9mo60JaUA"
-VIDEO_ID = "eURYSlBU1gk"
+VIDEO_ID = input("Enter the video ID: ")
 
 def fetch_comments(video_id, api_key):
     url = f"https://www.googleapis.com/youtube/v3/commentThreads"
@@ -20,4 +22,16 @@ def fetch_comments(video_id, api_key):
         print("Unable to fetch comments:", response.status_code, response.json())
         return []
 
-comments = fetch_comments(VIDEO_ID, API_KEY)
+def main():
+    comments = fetch_comments(VIDEO_ID, API_KEY)
+    if comments:
+        comment_texts = [comment['snippet']['topLevelComment']['snippet']['textDisplay'] for comment in comments]
+        for text in comment_texts:
+            print(f"{text}\n")
+        with open(f"{VIDEO_ID}_comments.json", "w") as f:
+            json.dump(comments, f, indent=2)
+        print("Comments saved to", f.name)
+
+
+if __name__ == "__main__":
+    main()
