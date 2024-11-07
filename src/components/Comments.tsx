f9584eaa40b167c898899be33f1faae357d0b052
TheKingsident <kingsident@gmail.com>
2024-11-07 23:52:12 +0100

    Refactor Comments component to use Headless UI for input fields and improve accessibility

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { useState } from 'react';
 import { fetchComments, Comment } from '../utils/fetchComments';
+import { Description, Field, Input, Label, Button } from '@headlessui/react'
+import clsx from 'clsx';
 
 const Comments = () => {
     const [comments, setComments] = useState<string[]>([]);
@@ -18,30 +20,40 @@ const Comments = () => {
 
     return (
         <div>
+            
             <h2>YouTube Comments</h2>
-            <div>
-                <label>Video ID:</label>
-                <input
-                    type="text"
+            <div className="input-field w-full max-w-md px-4">
+                <Field>
+                    <Label className="text-sm/6 font-medium text-white">Youtube Video ID</Label>
+                    <Description className="text-sm/6 text-white/50">Copy the link from your browser and paste it in the field below</Description>
+                    <Input
+                    className={clsx(
+                        'mt-3 block w-full rounded-lg border-none bg-white/5 py-1.5 px-3 text-sm/6 text-white',
+                        'focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25'
+                    )}
                     value={videoId}
                     onChange={(e) => setVideoId(e.target.value)}
-                    placeholder="Enter video ID"
-                    className='text-black'
-                />
-            </div>
-            <div>
-                <label>Keyword:</label>
-                <input
-                    type="text"
+                    />
+                </Field>
+                <Field className='mt-3'>
+                    <Label className="text-sm/6 font-medium text-white">Keyword</Label>
+                    <Description className="text-sm/6 text-white/50">What word are you searching for?</Description>
+                    <Input
+                    className={clsx(
+                        'mt-3 block w-full rounded-lg border-none bg-white/5 py-1.5 px-3 text-sm/6 text-white',
+                        'focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25'
+                    )}
                     value={keyword}
                     onChange={(e) => setKeyword(e.target.value)}
-                    placeholder="Enter keyword"
-                    className='text-black'
-                />
+                    />
+                </Field>
             </div>
-            <button onClick={handleFetchComments} disabled={loading}>
+
+
+            <Button className="inline-flex items-center gap-2 rounded-md mt-3 bg-gray-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white"
+                onClick={handleFetchComments} disabled={loading}>
                 {loading ? "Fetching Comments..." : "Fetch Comments"}
-            </button>
+            </Button>
             {comments.length > 0 ? (
                 <ul>
                     {comments.map((comment, index) => (
