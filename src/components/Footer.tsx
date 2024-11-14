2eda2c5717ab9a28aa96ee09f2a018d85b6a6414
TheKingsident <kingsident@gmail.com>
2024-11-14 17:34:03 +0100

    Update copyright notice in Footer component to reflect new branding

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <footer className="bg-gray-800 text-gray-400 py-4 mt-8">
             <div className="container mx-auto text-center px-4">
-                <p>&copy; {new Date().getFullYear()} Chattrix. All rights reserved.</p>
+                <p>&copy; {new Date().getFullYear()} Blue Wave Horizon. All rights reserved.</p>
                 <div className="mt-2 space-x-4">
                     <a href="https://x.com/CtrlAlt_Byte" target="_blank" rel="noopener noreferrer" className="hover:text-white">
                         Twitter
