528e64ae53feeb504bcf661c75fbf4f1cd2cb8d3
TheKingsident <kingsident@gmail.com>
2024-11-14 17:33:02 +0100

    Update social media links in Footer component for accuracy

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,13 +4,13 @@ const Footer = () => {
             <div className="container mx-auto text-center px-4">
                 <p>&copy; {new Date().getFullYear()} Chattrix. All rights reserved.</p>
                 <div className="mt-2 space-x-4">
-                    <a href="https://twitter.com/yourprofile" target="_blank" rel="noopener noreferrer" className="hover:text-white">
+                    <a href="https://x.com/CtrlAlt_Byte" target="_blank" rel="noopener noreferrer" className="hover:text-white">
                         Twitter
                     </a>
-                    <a href="https://github.com/yourprofile" target="_blank" rel="noopener noreferrer" className="hover:text-white">
+                    <a href="https://github.com/TheKingsident/" target="_blank" rel="noopener noreferrer" className="hover:text-white">
                         GitHub
                     </a>
-                    <a href="https://www.linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer" className="hover:text-white">
+                    <a href="https://www.linkedin.com/in/thekingsident/" target="_blank" rel="noopener noreferrer" className="hover:text-white">
                         LinkedIn
                     </a>
                 </div>
