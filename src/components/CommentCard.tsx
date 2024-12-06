522aa561766eb69171577d62f8b29ff14b65120c
TheKingsident <kingsident@gmail.com>
2024-12-06 06:23:27 +0100

    Refactor CommentCard to use state for author profile image and handle image loading errors

--- src/components/CommentCard.tsx
+++ src/components/CommentCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BiLike } from "react-icons/bi";
 import { BsReply } from "react-icons/bs";
 import { Comment } from '@/utils/fetchComments'
@@ -11,32 +11,26 @@ interface CommentCardProps {
 
 const CommentCard: React.FC<CommentCardProps> = ({comment }) => {
     const snippet = comment.snippet?.topLevelComment?.snippet;
+    const [imgSrc, setImgSrc] = useState(snippet?.authorProfileImageUrl || '/profile.svg');
     if (!snippet) return null;
-    console.log(snippet.authorProfileImageUrl)
+
+    const handleError = () => {
+        setImgSrc('/profile.svg');
+    }
 
 
     return (
         <div className="flex flex-col bg-gray-300 p-4 rounded shadow-lg space-y-3">
             <div className="flex items-center space-x-4">
-                {snippet.authorProfileImageUrl ? (
-                    <Image
-                        className="w-12 h-12 rounded-full object-cover"
-                        src={snippet.authorProfileImageUrl}
-                        alt={`${snippet.authorDisplayName}'s pic`}
-                        width={48}
-                        height={48}
-                        onError={(e) => {
-                            e.currentTarget.src = '/profile.svg';
-                        }}
-                    />
-                ) : (
-                    <img
-                        className="w-12 h-12 rounded-full object-cover"
-                        src="/profile.svg"
-                        alt={`${snippet.authorDisplayName}'s pic`}
-                    />	
-                )}
-
+                <Image
+                    className="w-12 h-12 rounded-full object-cover"
+                    src={snippet.authorProfileImageUrl}
+                    alt={`${snippet.authorDisplayName}'s pic`}
+                    width={48}
+                    height={48}
+                    unoptimized
+                    onError={handleError}
+                />
                 <div>
                     <h3 className="text-sm font-semibold">{snippet.authorDisplayName}</h3>
                     <p className="text-xs text-gray-600">Posted on {new Date(snippet.publishedAt).toLocaleDateString()}</p>
